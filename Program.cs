8536e7fead17b4ce4c75c5a438499c1da3b13407
fres-sudo <francescocalicchio@hotmail.com>
2025-02-03 19:26:48 +0100

    [FIX] container port

--- Program.cs
+++ Program.cs
@@ -2,15 +2,16 @@ using CurrencyExchangeService.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
 builder.Services.AddHttpClient<IExchangeRateService, ExchangeRateService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseSwagger();
+    app.UseSwaggerUI();
     app.UseDeveloperExceptionPage();
 }
 
